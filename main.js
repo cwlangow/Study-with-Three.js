5b0b1f3af718412107eafc96474b98ded71d39e0
cwlangow <calangowproducer@gmail.com>
2026-01-08 16:07:21 -0300

    Refactor code structure for improved readability and maintainability

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 div3d.appendChild(renderer.domElement)
 
 loader.load(
-    'apartment.glb',
+    './assets/apartment.glb',
     (gltf) => {
         console.log('Modelo carregado')
         root = gltf.scene
